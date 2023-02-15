99096c85e937f1c968a2dd0e6318558393ca1952
AlzahraniBushra <125342572+AlzahraniBushra@users.noreply.github.com>
2023-02-15 14:29:17 +0300

    Update Book.js

--- src/Comp/Book.js
+++ src/Comp/Book.js
@@ -22,7 +22,7 @@ const Book = ({ book, setBooks }) => {
                         width: 128,
                         height: 193,
                         backgroundImage:
-                            `url(${book.imageLinks.thumbnail})`,
+                           book.imageLinks && `url(${book.imageLinks.thumbnail})`,
                     }}
                 ></div>
                 <div className="book-shelf-changer">
